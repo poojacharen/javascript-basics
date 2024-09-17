cdce87062da4a6047782fc58443289edbb73c1d2
poojaraju23 <poojaraju23@gmail.com>
2024-09-17 11:12:58 -0400

    Dynamic Typing: 2 types - static and dynamic and its used with (typeof)

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ let age = 29; //Number Literal
 let isApproved = true; //Boolean Literal - can be true or false
 let firstName = undefined;
 let selectedColor = null; //If the user selects a color we can assign a color name instead of null, if not selected then it is a null
+
+/* Dynamic Typing */
+//JS is a dynamic language
+//Two types of languages : Static and Dynamic languages
+//In static variable, the type of the variable is set and cannot be changed in the future 
+//In dynamic variable like JS, the type of variable can be changed in runtime

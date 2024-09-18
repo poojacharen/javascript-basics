803d6ff02589ed22b2e1be06750960be04290a8c
poojaraju23 <poojaraju23@gmail.com>
2024-09-18 13:27:20 -0400

    Bitwise operators

--- index.js
+++ index.js
@@ -174,5 +174,36 @@ let currentColor = userColor || defaultColor;
 
 console.log(currentColor);
 
+/* Bitwise operators */
+
+// 1 = 00000001
+// 2 = 00000010
+// 3 = 00000011
+// R = 00000000
+
+console.log(1 | 2);  // Bitwise OR
+console.log(1 & 2);  // Bitwise AND
+
+// Read, Write, Execute
+// 00000100
+// 00000010
+// 00000001
+
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+
+let myPermission = 0;
+myPermission = myPermission | writePermission;
+
+let message = (myPermission & readPermission) ? 'yes' : 'no';
+
+console.log(message);
+
+
+
+
+
+
 
 

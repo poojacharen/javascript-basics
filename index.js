64d6d74ff2576eb33bb24b68b231be9f91e43c58
poojaraju23 <poojaraju23@gmail.com>
2024-09-19 10:02:38 -0400

    Switch...case and also wrote the code with if...else

--- index.js
+++ index.js
@@ -241,6 +241,27 @@ else
     console.log('Good evevning');
 
 
+/* Switch...case */
+
+let role = 'guest';
+
+switch (role) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+
+        case 'moderator':
+            console.log('Moderator User');
+            break;
+
+            default:
+                console.log('Unknown User');
+}
+
+if (role === 'guest') console.log('Guest User');
+else if (role === 'moderator') console.log('Moderator User');
+else console.log('Unknown User');
+
 
 
 

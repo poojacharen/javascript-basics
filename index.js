320eb418fb7e74f4f984eff190737417be5ad58b
poojaraju23 <poojaraju23@gmail.com>
2024-09-19 09:49:55 -0400

    If...else

--- index.js
+++ index.js
@@ -224,6 +224,22 @@ console.log(e);
 console.log(f);
 
 
+/* If...else */
+
+// Hour
+// If hour is between 6am and 12pm: Good morning!
+// If it is between 12pm and 6pm: Good afternoon!
+// Otherwise: Good evening!  (24hr format)
+
+let hour = 14;
+
+if (hour >= 6 && hour < 12)
+    console.log('Good morning');
+else if (hour >= 12 && hour < 18)
+    console.log('Good afternoon');
+else
+    console.log('Good evevning');
+
 
 
 

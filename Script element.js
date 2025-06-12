c40c6e3f4e270ce882fd1034c7d7d26c423f1911
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-06-12 16:51:59 -0400

    Create Script element

---
+++ Script element.js
@@ -0,0 +1,18 @@
+-- script element should be given only after the body section
+-- Reason : refer codewithmosh
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Hello Pooja !!!</h1>
+    <script>
+      // My first JavaScript code
+      console.log("Hello Pooja !!!");      -- Added the script element 
+    </script>
+  </body>
+</html>

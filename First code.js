b8c9fecf40f030b899971058a90d105e9f524704
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-06-12 16:43:35 -0400

    Create First code

---
+++ First code.js
@@ -0,0 +1,11 @@
+-- In chrome browser --> inspect --> console
+
+console.log('Hello World');
+Op : Hello World
+
+-- ALso, we can write mathematical expressions
+2 + 2
+Op : 4
+
+alert('yo')
+Op : We get a pop up saying yo

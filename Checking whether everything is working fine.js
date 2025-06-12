09116c1bf458b3cbfb1f3c203ef26511b281d9ea
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-06-12 16:45:55 -0400

    Create Checking whether everything is working fine with VS code and Live Server

---
+++ Checking whether everything is working fine.js
@@ -0,0 +1,17 @@
+-- Created a folder called JavaScript in the desktop
+-- Opened the folder in VS code --> created a file named index.html
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Hello Pooja !!!</h1>
+  </body>
+</html>
+
+
+-- Checked with live server whether everything is working fine
